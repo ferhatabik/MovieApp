13221d3235f9187ea9f0ebac040141162dd03f4d
Ferhat Abik <ferhatabik90@gmail.com>
2023-09-19 13:56:40 +0300

    bir iki değişiklik yapıldı

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 // details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
 
 // HTML'den gerekli elementleri alıyoruz
-const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list');
-const resultGrid = document.getElementById('result-grid');
+const movieSearchBox = document.getElementById('movie-search-box'); // Arama kutusu
+const searchList = document.getElementById('search-list'); // Arama sonuçları listesi
+const resultGrid = document.getElementById('result-grid'); // Film detaylarının gösterildiği alan
 
 // API'den filmleri yüklemek için async bir fonksiyon oluşturuyoruz
 async function loadMovies(searchTerm) {
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
-    const res = await fetch(URL);
-    const data = await res.json();
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`; // OMDB API'ye istek yapmak için URL
+    const res = await fetch(URL); // API'ye istek gönderme
+    const data = await res.json(); // API yanıtını JSON olarak ayrıştırma
     if (data.Response == "True") {
         // Eğer API yanıtı başarılıysa, filmleri listelemek için displayMovieList fonksiyonunu çağırıyoruz
         displayMovieList(data.Search);
@@ -19,23 +19,23 @@ async function loadMovies(searchTerm) {
 
 // Kullanıcının film aramasını işleyen fonksiyon
 function findMovies() {
-    let searchTerm = (movieSearchBox.value).trim();
+    let searchTerm = (movieSearchBox.value).trim(); // Arama kutusundan kullanıcının girdiği metni al
     if (searchTerm.length > 0) {
-        searchList.classList.remove('hide-search-list');
-        loadMovies(searchTerm);
+        searchList.classList.remove('hide-search-list'); // Arama sonuçlarını göstermek için CSS sınıfını kaldır
+        loadMovies(searchTerm); // Filmleri yükle
     } else {
-        searchList.classList.add('hide-search-list');
+        searchList.classList.add('hide-search-list'); // Arama sonuçlarını gizlemek için CSS sınıfını ekle
     }
 }
 
 // Film listesini ekranda gösteren fonksiyon
 function displayMovieList(movies) {
-    searchList.innerHTML = "";
+    searchList.innerHTML = ""; // Arama sonuçları listesini temizle
     for (let idx = 0; idx < movies.length; idx++) {
-        let movieListItem = document.createElement('div');
+        let movieListItem = document.createElement('div'); // Her film için bir div öğesi oluştur
         movieListItem.dataset.id = movies[idx].imdbID; // movie id'sini data-id olarak ayarlıyoruz
-        movieListItem.classList.add('search-list-item');
-        let moviePoster = (movies[idx].Poster != "N/A") ? movies[idx].Poster : "image_not_found.png";
+        movieListItem.classList.add('search-list-item'); // CSS sınıfını ekleyerek stil uygula
+        let moviePoster = (movies[idx].Poster != "N/A") ? movies[idx].Poster : "image_not_found.png"; // Film posterini kontrol et
         movieListItem.innerHTML = `
         <div class = "search-item-thumbnail">
             <img src = "${moviePoster}">
@@ -44,22 +44,22 @@ function displayMovieList(movies) {
             <h3>${movies[idx].Title}</h3>
             <p>${movies[idx].Year}</p>
         </div>
-        `;
-        searchList.appendChild(movieListItem);
+        `; // HTML içeriğini oluştur
+        searchList.appendChild(movieListItem); // Film öğesini arama sonuçları listesine ekleyin
     }
     loadMovieDetails(); // Film detaylarını yükle
 }
 
 // Her bir film öğesine tıklamada film detaylarını yükleme işlemini gerçekleştiren fonksiyon
 function loadMovieDetails() {
-    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    const searchListMovies = searchList.querySelectorAll('.search-list-item'); // Tüm film öğelerini seç
     searchListMovies.forEach(movie => {
         movie.addEventListener('click', async () => {
-            searchList.classList.add('hide-search-list');
-            movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
-            const movieDetails = await result.json();
-            displayMovieDetails(movieDetails);
+            searchList.classList.add('hide-search-list'); // Arama sonuçlarını gizle
+            movieSearchBox.value = ""; // Arama kutusunu temizle
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`); // Film detayları için OMDB API'ye istek gönder
+            const movieDetails = await result.json(); // API yanıtını JSON olarak ayrıştırma
+            displayMovieDetails(movieDetails); // Film detaylarını gösterme
         });
     });
 }
